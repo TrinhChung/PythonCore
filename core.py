0ae0e4a8147eeb77cdaa2417142086f6ed501e46
TrinhChung <chungtrinh2k2@gmail.com>
2023-06-27 14:50:28 +0700

    change text

---
+++ core.py
@@ -0,0 +1,12 @@
+import module;
+import os;
+import datetime;
+import math;
+grade1 = 80
+grade2 = 90
+average = (grade1 + grade2) / 2
+k = 2 + 3j
+x = 1.0
+y = 1.0
+print(math.trunc(20.3333))
+eval
